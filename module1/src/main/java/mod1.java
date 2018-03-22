66a8f50b597d01ae3994bfa57b2d1a829b4389d8
sbt-budaev-av <budaevav@gmail.com>
2018-03-22 21:16:56 +0300

    Initial commit

---
+++ module1/src/main/java/mod1.java
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+public class mod1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ module2/src/main/java/mod2.java
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+public class mod2 {
+    public static void main(String[] args) {
+    System.out.println("Hello World!");
+}
+}
