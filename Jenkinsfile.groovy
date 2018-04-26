17b938644fae0f539f8150fde2b8577e6c365b55
Alexander Budaev <budaevav@gmail.com>
2018-04-26 07:54:46 +0300

    add param

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -28,7 +28,7 @@ pipeline {
             steps {
                 script {
                     timeout(time: 1, unit: 'HOURS') {
-                        def qg = waitForQualityGate()
+                        def qg = waitForQualityGate([authToken:'d40041535f4bdbb44bace3803171331b06d78315'])
                         if (qg.status != 'OK') {
                             error "Pipeline aborted due to quality gate failure: ${qg.status}"
                         }
